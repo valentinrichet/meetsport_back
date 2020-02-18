577b256e0c0d83ae2d2afc9282ac5f09c4951716
Dark3DVFX <valentin.richet@gmail.com>
2020-02-18 15:17:47 +0100

    Added Swagger

---
+++ Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetSport
+{
+    public class Settings
+    {
+        public const string CONNECTION_STRING_URL = "MeetSportUrl";
+        public const string CONNECTION_STRING_VERSION = "MeetSportVersion";
+        public const string SWAGGER = "Swagger";
+        public const string SWAGGER_ENDPOINT = "EndPoint";
+        public const string SWAGGER_VERSION = "Version";
+        public const string SWAGGER_TITLE = "Title";
+        public const string SWAGGER_DESCRIPTION = "Description";
+        public const string SWAGGER_CONTACT = "Contact";
+        public const string SWAGGER_CONTACT_NAME = "Name";
+        public const string SWAGGER_CONTACT_EMAIL = "Email";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore;
 using MeetSport.Models;
+using Microsoft.OpenApi.Models;
 
 namespace MeetSport
 {
@@ -28,11 +29,31 @@ namespace MeetSport
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            /* Configure Database */
             services.AddDbContext<projetContext>(optionsBuilder =>
             {
-                optionsBuilder.UseMySql(Configuration.GetConnectionString("MeetSportUrl"), x => x.ServerVersion(Configuration.GetConnectionString("MeetSportVersion")));
+                optionsBuilder.UseMySql(Configuration.GetConnectionString(Settings.CONNECTION_STRING_URL), x => x.ServerVersion(Configuration.GetConnectionString(Settings.CONNECTION_STRING_VERSION)));
             });
-               
+
+            /* Configure Swagger */
+            services.AddSwaggerGen(c =>
+            {
+                IConfigurationSection swaggerSection = Configuration.GetSection(Settings.SWAGGER);
+                IConfigurationSection swaggerContactSection = swaggerSection.GetSection(Settings.SWAGGER_CONTACT);
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = swaggerSection.GetValue<string>(Settings.SWAGGER_VERSION),
+                    Title = swaggerSection.GetValue<string>(Settings.SWAGGER_TITLE),
+                    Description = swaggerSection.GetValue<string>(Settings.SWAGGER_DESCRIPTION),
+                    Contact = new OpenApiContact
+                    {
+                        Name = swaggerContactSection.GetValue<string>(Settings.SWAGGER_CONTACT_NAME),
+                        Email = swaggerContactSection.GetValue<string>(Settings.SWAGGER_CONTACT_EMAIL)
+                    }
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -45,6 +66,18 @@ namespace MeetSport
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                IConfigurationSection swaggerSection = Configuration.GetSection(Settings.SWAGGER);
+
+                c.SwaggerEndpoint(swaggerSection.GetValue<string>(Settings.SWAGGER_ENDPOINT), $"{swaggerSection.GetValue<string>(Settings.SWAGGER_TITLE)} {swaggerSection.GetValue<string>(Settings.SWAGGER_VERSION)}");
+            });
 
             app.UseRouting();
 
